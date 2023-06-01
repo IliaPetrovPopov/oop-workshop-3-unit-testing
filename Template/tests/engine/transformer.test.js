8c25434d67e4e3782e15b7eff5afd75544e59e39
Ilia Popov <ilia_popov02@abv.bg>
2023-06-01 08:49:35 +0300

    Add unit tests for transformer.js

--- Template/tests/engine/transformer.test.js
+++ Template/tests/engine/transformer.test.js
@@ -1,1 +1,22 @@
+import { Transformer } from '../../src/engine/transformer.js';
 
+describe('transformer', () => {
+
+  it('Transform method should call the passed function ', () => {
+    const transformerTest = new Transformer();
+    const mockTransformer = jest.fn(num => num * 2);
+
+    transformerTest.transform([5, 6], mockTransformer);
+
+    expect(mockTransformer).toHaveBeenCalled();
+  });
+
+  it('Transform method should return the correct value, i.e. the transformed array', () => {
+    const transformerTest = new Transformer();
+    const mockTransformer = jest.fn(num => num * 2);
+
+    const resultTest = transformerTest.transform([5, 6], mockTransformer);
+
+    expect(resultTest).toEqual([10, 12]);
+  });
+});
