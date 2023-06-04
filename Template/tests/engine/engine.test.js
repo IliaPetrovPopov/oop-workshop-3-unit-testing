d9b3a8bb35ceeeb46d63e818434937fb14acdc28
Ilia Popov <ilia_popov02@abv.bg>
2023-06-04 20:59:59 +0300

    Add engine tests

--- Template/tests/engine/engine.test.js
+++ Template/tests/engine/engine.test.js
@@ -1,1 +1,49 @@
+import { Engine } from '../../src/engine/engine.js';
+import { fileReader, transformer, logger } from '../../src/engine/mock-calls.js';
 
+describe('Engine', () => {
+
+  describe('run', () => {
+
+    it('should call the readFile method', () => {
+
+      const spy = jest.spyOn(fileReader, 'readFile');
+
+      const engine = new Engine(fileReader, transformer, logger);
+
+      engine.run();
+
+      expect(spy).toHaveBeenCalled();
+      expect(fileReader.readFile).toHaveBeenCalled();
+      spy.mockClear();
+    });
+
+    it('should call the transform method', () => {
+
+      const spy = jest.spyOn(transformer, 'transform');
+
+      const engine = new Engine(fileReader, transformer, logger);
+
+      engine.run();
+
+      expect(transformer.transform).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalled();
+
+      spy.mockClear();
+    });
+
+    it('should call logWith method', () => {
+
+      const spy = jest.spyOn(logger, 'logWith');
+
+      const engine = new Engine(fileReader, transformer, logger);
+
+      engine.run();
+
+      expect(spy).toHaveBeenCalled();
+      spy.mockClear();
+    });
+
+  });
+
+});
